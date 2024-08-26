0ed5d0b54ce2088312d670f2b7edcc36a7f55ef6
jeanrenejaouen <jean-rene.jaouen@wanadoo.fr>
2024-08-26 12:20:08 +0200

    Updates

---
+++ precache-manifest.aa123809132ef7fc988b27dfbdf57e8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d66ed0c91ebdf0b743b528c14ef351a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fab32cca12b6e7ed768",
+    "url": "/static/css/main.227412f1.chunk.css"
+  },
+  {
+    "revision": "b05f0391601814739f2f",
+    "url": "/static/js/2.7e38060a.chunk.js"
+  },
+  {
+    "revision": "cbf92ed71fb4f2e94692ba67215d89b7",
+    "url": "/static/js/2.7e38060a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fab32cca12b6e7ed768",
+    "url": "/static/js/main.f33f1803.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "3d5fc962a81efc2214e8befb971ac275",
+    "url": "/static/media/imgBanner.3d5fc962.png"
+  },
+  {
+    "revision": "4e667dc2a37c0e2e083419531ac8b0ef",
+    "url": "/static/media/photo-profil.4e667dc2.png"
+  }
+]);
